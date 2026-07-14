f1ca6f28eedba77e936a235c4ddeebd81ce42d8b
Carlos-Segundo <segundocarlosalexandre@gmail.com>
2026-07-14 08:23:16 -0400

    feat: adding the end points

---
+++ Stopwatch-Front/src/App.jsx
@@ -0,0 +1,99 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+
+  const [status, setStatus] = useState('Idle')
+  const [elapsedTime, setElapsedTime] = useState('00:00:00')
+
+  useEffect(() => {
+    fetch('http://localhost:5168/api/stopwatch/status')
+      .then(response => response.json())
+      .then(data => {
+        setStatus(data.state)
+        setElapsedTime(data.elapsedTime)
+      })
+      .catch(error => {
+        console.error('Erro ao acessar a API:', error)
+      })
+  }, [])
+
+
+  useEffect(() => {
+    if (status !== 'Running') return
+
+    const interval = setInterval(() => {
+      fetch('http://localhost:5168/api/stopwatch/status')
+        .then(response => response.json())
+        .then(data => {
+          setStatus(data.state)
+          setElapsedTime(data.elapsedTime)
+        })
+    }, 100)
+
+    return () => clearInterval(interval)
+  }, [status])
+
+
+  function handle(){
+    fetch('http://localhost:5168/api/stopwatch/start', {
+      method: 'POST'
+  })
+    .then(response => response.json())
+    .then(() => {
+      setStatus('Running')
+    })
+    .catch(error => {
+      console.error('Erro ao acessar a API:', error)
+    })
+  }
+
+  function handle(){
+    fetch('http://localhost:5168/api/stopwatch/paused', {
+      method: 'POST'
+  })
+    .then(response => response.json())
+    .then(() => {
+      setStatus('Paused')
+    })
+    .catch(error => {
+      console.error('Erro ao acessar a API:', error)
+    })
+  }
+
+  function handle(){
+    fetch('http://localhost:5168/api/stopwatch/reset', {
+      method: 'POST'
+  })
+    .then(response => response.json())
+    .then(() => {
+      setStatus('Idle')
+    })
+    .catch(error => {
+      console.error('Erro ao acessar a API:', error)
+    })
+  }
+
+  return (
+    <main>
+      <h1>Stopwatch</h1>
+
+      <h2>{elapsedTime}</h2>
+
+      <p>Status: {status}</p>
+
+      <button onClick={handle}>
+        Start
+      </button>
+      <button onClick={handle}>
+        Pause
+      </button>
+      <button onClick={handle}>
+        Reset
+      </button>
+    </main>
+
+  )
+}
+
+export default App
